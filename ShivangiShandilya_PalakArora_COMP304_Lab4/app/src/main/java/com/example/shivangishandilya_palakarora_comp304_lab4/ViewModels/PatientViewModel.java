5b1a7e0e38140e6be515d0171cb92392fcb27bb0
arorpalak <60004971+arorpalak@users.noreply.github.com>
2021-01-05 22:20:21 -0500

    COMP304 Mobile Application Development
    Lab4

---
+++ ShivangiShandilya_PalakArora_COMP304_Lab4/app/src/main/java/com/example/shivangishandilya_palakarora_comp304_lab4/ViewModels/PatientViewModel.java
@@ -0,0 +1,44 @@
+package com.example.shivangishandilya_palakarora_comp304_lab4.ViewModels;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+import com.example.shivangishandilya_palakarora_comp304_lab4.Models.Patient;
+import com.example.shivangishandilya_palakarora_comp304_lab4.Models.PatientRepository;
+
+import java.util.List;
+
+public class PatientViewModel extends AndroidViewModel {
+    private PatientRepository patientRepository;
+
+    //private List<Patient> allPatients;
+
+
+    public PatientViewModel(@NonNull Application application) {
+        super(application);
+        this.patientRepository = new PatientRepository(application);
+   //     this.allPatients = patientRepository.AllPatients();
+    }
+
+    public void insert(Patient patient){
+        patientRepository.insert(patient);
+    }
+    public void update(Patient patient){
+        patientRepository.update(patient);
+    }
+
+    public void delete(Patient patient){
+        patientRepository.delete(patient);
+    }
+
+    public List<Patient> allPatients(){
+        return patientRepository.AllPatients();
+    }
+
+    public Patient getPatientById(int patientId)
+    {
+        return patientRepository.getPatientById(patientId);
+    }
+}
